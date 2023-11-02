32d8f773d3186a659ef377ee06f20b3503d9b7ea
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-11-02 16:23:42 +0100

    update to production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 import { IEnvironment } from "src/app/shared/interfaces/i-environment";
 
 export const environment: IEnvironment = {
-	production: false,
+	production: true,
 	apiEnabled: false,
 	feature: {
 		dashboard: false,
-		map: false,
+		map: true,
 	},
 	text: 'This is production environment',
 	apiUrl: 'https://www.matthijskamstra.nl/expenses/',
