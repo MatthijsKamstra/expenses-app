78c28f56ca9e6a7988872e73a48c4d2aa8713a68
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-11-15 10:27:45 +0100

    remove log

--- src/app/pages/locations-page/locations-page.component.ts
+++ src/app/pages/locations-page/locations-page.component.ts
@@ -52,7 +52,7 @@ export class LocationsPageComponent implements OnInit {
 				console.warn(error);
 			},
 			complete: () => {
-				console.info('done');
+				// console.info('done');
 			}
 		});
 	}
